909354f8786da08c56de81668bc0c77eab17b9d8
HayaOsama224 <h.osama2298@nu.edu.eg>
2025-07-16 04:21:17 +0300

    Add my skills&projects&About me

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,19 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rjshkhr.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: '',
+  title: 'Junior Developer',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Haya Osama Nabil',
+  role: 'Backend Developer intern',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+     "I'm a Senior computer science student and backend developer intern passionate about building robust, scalable APIs and modern backend systems. I enjoy working with python, SQL, and clean architecture principles to create reliable solutions.",
+  // resume: 'https://example.com',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/haya-osama-nabil/?trk=public-profile-join-page',
+    github: 'https://github.com/HayaOsama224',
   },
 }
 
@@ -21,29 +21,29 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+     name: 'Taqyim',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'A Yelp-like platform to rate and review nearby places using React, .NET, and SQL Server.',
+    stack: ['React', 'ASP.NET Core', 'SQL Server'],
+    sourceCode: 'https://github.com/iMalakAhmed/Taqyim',
+   // livePreview: 'https://github.com',
   },
   {
-    name: 'Project 2',
+    name: 'Emoji Detection',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Classifies emoji expressions using CNN and OpenCV preprocessing techniques.',
+    stack: ['Python', 'TensorFlow', 'OpenCV'],
+    sourceCode: 'https://github.com/iMalakAhmed/Emoji-Detection-System',
+    // livePreview: '',
   },
   {
-    name: 'Project 3',
+    name: 'Portfolio',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'A personal portfolio website showcasing my projects and skills.',
+    stack: ['React', 'CSS'],
+    // sourceCode: ' 
   },
+
 ]
 
 const skills = [
@@ -54,18 +54,20 @@ const skills = [
   'JavaScript',
   'TypeScript',
   'React',
-  'Redux',
-  'SASS',
-  'Material UI',
+  'Problem Solving',
+  'Machine Learning',
   'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
+  'Github',
+  'Java',
+  'C# .NET ',
+  'Agile Methodologies',
+  'SQL',
+  'Python',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'h.osama2298@mail.com',
 }
 
 export { header, about, projects, skills, contact }
